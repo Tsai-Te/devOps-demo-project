beb17d9a4e89a088a31df7ec2f1a1180970df840
Te <tsai.te0210@gmail.com>
2023-08-10 23:58:16 -0400

    first commit

---
+++ src/main/java/com/devOpsdemo/devOpsdemoproject/DevOpsDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.devOpsdemo.devOpsdemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsDemoProjectApplication.class, args);
+	}
+
+}
